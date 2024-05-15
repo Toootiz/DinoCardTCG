81eaf3b7b4d2b75e94999c03696d94f30aa42e53
Nikole Mrs <nikole@Nikoles-MacBook-Pro.local>
2024-05-15 13:51:01 -0600

    unity screens

---
+++ Videogame/Unity/Dinosaur_Game/Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir(){
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+}
