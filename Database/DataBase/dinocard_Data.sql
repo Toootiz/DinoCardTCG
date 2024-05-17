52d226b69ff7bdc43dc1ebddb716398e59aa2f85
Gabriel <Toootiz.killer@gmail.com>
2024-05-16 21:37:36 -0600

    Fixed database error

---
+++ Database/DataBase/dinocard_Data.sql
@@ -0,0 +1,44 @@
+USE `dinocard_db`;
+
+INSERT INTO habilidad (descripcion) VALUES 
+('Habilidad 1'),
+('Habilidad 2'),
+('Habilidad 3'),
+('Habilidad 4'),
+('Habilidad 5'),
+('Habilidad 6'),
+('Habilidad 7'),
+('Habilidad 8');
+
+INSERT INTO carta (`Nombre`, `Puntos_de_Vida`, `Puntos_de_ataque`, `Coste_en_elixir`, `Habilidad`) VALUES
+('T-Rex', 11, 6, 7, 2),
+('Compy', 1, 1, 1, 1),
+('Spinosaurus', 6, 4, 5, 1),
+('Argentavis', 8, 4, 4, 1),
+('Brontosaurus', 14, 4, 5, 1),
+('Stegosaurus', 10, 3, 6, 3),
+('Ankylosaurus', 3, 4, 4, 1),
+('Allosaurus', 7, 5, 6, 4),
+('Carnotaurus', 6, 4, 4, 1),
+('Pteranodon', 6, 4, 3, 1),
+('Triceratops', 12, 3, 3, 1),
+('Velociraptor', 6, 3, 4, 1),
+('Mosasaurus', 12, 4, 5, 1),
+('Megalodon', 10, 5, 6, 5),
+('Therizinosaurus', 8, 3, 4, 8),
+('Giganotosaurus', 12, 8, 7, 1),
+('Diplodocus', 10, 2, 4, 1),
+('Parasaurolophus', 4, 2, 4, 1),
+('Sabertooth', 6, 4, 3, 1),
+('Woolly Mammoth', 12, 4, 5, 1),
+('Direwolf', 6, 4, 3, 1),
+('Baryonyx', 6, 4, 4, 1),
+('Dodo', 3, 2, 1, 1),
+('Kaprosuchus', 10, 5, 7, 1),
+('Oviraptor', 2, 1, 3, 6),
+('Giganoto', 14, 7, 7, 1),
+('Gallimimus', 4, 2, 3, 7),
+('Pulmonoscorpius', 8, 4, 4, 1),
+('Archaeopteryx', 2, 1, 3, 1);
+
+
